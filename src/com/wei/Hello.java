40447615ad9af08eb45cb3c13d441552566247b2
weierdong <weierdong@yupiao.com>
2017-06-22 11:28:42 +0800

    Initial commit

---
+++ src/com/wei/Hello.java
@@ -0,0 +1,10 @@
+package com.wei;
+
+/**
+ * Created by Administrator on 2017/6/21.
+ */
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("asdf");
+    }
+}
